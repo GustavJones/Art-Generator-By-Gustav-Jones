15e5627f40f32f9f0a39e7700891e654e5efd13e
GustavJones <75836198+GustavJones@users.noreply.github.com>
2021-12-13 18:37:29 +0000

    Added compulsory "results" folder and created a sample image

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 config = {
   "Delete Files": False, # If True, no images will be generated but images will be deleted
-  "Images Amount": 10, 
+  "Images Amount": 1, 
   "Image Size": 720,
   "Max Sides": 6,
   "Shapes Amount": 10
